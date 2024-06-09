ca9c3c18c59bc68503556b1fc6a22f4ab42ff10b
kumar10255 <kkumar10255@gmail.com>
2024-06-09 13:41:05 +0000

    blog list application

---
+++ src/components/BlogItem/index.js
@@ -0,0 +1,19 @@
+
+import './index.css'
+
+const BlogItem = props => {
+  const {blogDetails} = props
+  const {title, description, publishedDate} = blogDetails
+
+  return (
+    <li className="blog-item">
+      <div className="blog-details-container">
+        <h1 className="blog-title">{title}</h1>
+        <p className="blog-published-date">{publishedDate}</p>
+      </div>
+      <p className="blog-description">{description}</p>
+    </li>
+  )
+}
+
+export default BlogItem
